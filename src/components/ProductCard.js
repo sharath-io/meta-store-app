d789572cb5046a72cd0756c43d166b25c5c6cf5e
Sharath <sharath.esr@gmail.com>
2024-08-17 22:04:43 +0530

    style: update styling in product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,31 +3,38 @@ import { ProductContext } from "../contexts/ProductContext";
 import { useNavigate } from "react-router-dom";
 
 export function ProductCard({ product }) {
-  const { state,dispatch } = useContext(ProductContext);
+  const { state, dispatch } = useContext(ProductContext);
   const navigate = useNavigate();
   return (
     <>
       <li className="product-item card-item">
-        <h4>{product.title}</h4>
+        <p className="title">{product.title}</p>
         <img src={product.image} alt={product.title} className="product-img" />
+
+        <p className="category">{product.categoryName}</p>
+        <p><span>${product.originalPrice}</span>  $ {product.sellingPrice} USD</p>
+        <p>rating: {product.rating}</p>
+
         <div>
-          {
-            state.cart.find(item => item._id===product._id) 
-            ? <button
-            className="card-button go-to-cart"
-            onClick={()=> navigate('/cart')}>
-            Go to Cart
-          </button>
-          :
-          <button
-            className="card-button"
-            onClick={() =>
-              dispatch({ type: "ADD_TO_CART", payload: product._id })
-            }
-          >
-            Add to Cart
-          </button>}
+          {state.cart.find((item) => item._id === product._id) ? (
+            <button
+              className="card-button go-to-cart"
+              onClick={() => navigate("/cart")}
+            >
+              Go to Cart
+            </button>
+          ) : (
+            <button
+              className="card-button"
+              onClick={() =>
+                dispatch({ type: "ADD_TO_CART", payload: product._id })
+              }
+            >
+              Add to Cart
+            </button>
+          )}
         </div>
+
       </li>
     </>
   );
