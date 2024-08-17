c304d3d6e14fd5aaebe63abdeaa65a9376222b03
Sharath <sharath.esr@gmail.com>
2024-08-17 20:10:57 +0530

    feat: navigate to cart if product is already in cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,21 +1,34 @@
-import { useContext } from "react"
-import { ProductContext } from "../contexts/ProductContext"
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import { useNavigate } from "react-router-dom";
 
-export function ProductCard({product}){
-  const {dispatch} = useContext(ProductContext)
-    return (
-        <>
-          <li className="product-item card-item">
-            <h4>{product.title}</h4>
-            <img
-              src={product.image}
-              alt={product.title}
-              className="product-img"
-            />
-            <div>
-              <button className="card-button" onClick={()=> dispatch({type:'ADD_TO_CART', payload:product._id})}>Add to Cart</button>
-            </div>
-          </li>
-        </>
-    )
+export function ProductCard({ product }) {
+  const { state,dispatch } = useContext(ProductContext);
+  const navigate = useNavigate();
+  return (
+    <>
+      <li className="product-item card-item">
+        <h4>{product.title}</h4>
+        <img src={product.image} alt={product.title} className="product-img" />
+        <div>
+          {
+            state.cart.find(item => item._id===product._id) 
+            ? <button
+            className="card-button go-to-cart"
+            onClick={()=> navigate('/cart')}>
+            Go to Cart
+          </button>
+          :
+          <button
+            className="card-button"
+            onClick={() =>
+              dispatch({ type: "ADD_TO_CART", payload: product._id })
+            }
+          >
+            Add to Cart
+          </button>}
+        </div>
+      </li>
+    </>
+  );
 }
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -3,17 +3,21 @@ import { products } from "../db/products";
 
 export const ProductContext = createContext();
 
-
-const reducerFn =(state,action) => {
-    switch(action.type){
-        case 'ADD_TO_CART':
-            return {
-                ...state,cart:[...state.cart, state.products.find(product => product._id===action.payload)]
-            }
-        default: return state
+const reducerFn = (state, action) => {
+    switch (action.type) {
+      case "ADD_TO_CART":
+        return {
+          ...state,
+          cart: [
+            ...state.cart,
+            state.products.find((product) => product._id === action.payload),
+          ],
+        };
+      default:
+        return state;
     }
-
-}
+  };
+  
 
 export function ProductProvider({children}){
 
