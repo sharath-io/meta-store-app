30c6aecabbf91d8229b0b1d353f13c91e15b68ed
Sharath <sharath.esr@gmail.com>
2024-08-17 19:21:52 +0530

    feat: display products in product listing page by importing

--- src/pages/ProductListing.js
+++ src/pages/ProductListing.js
@@ -1,5 +1,26 @@
-export function ProductListing(){
-    return (
-        <h1>Product Listing Page</h1>
-    )
+import { products } from "../db/products";
+export function ProductListing() {
+  return (
+    <>
+      <h1>Product Listing Page</h1>
+
+      <ul className="card-container">
+        {
+         products.map((product) => (
+          <li className="product-item card-item">
+            <h4>{product.title}</h4>
+            <img
+              src={product.image}
+              alt={product.title}
+              className="product-img"
+            />
+            <div>
+              <button className="card-button">Add to Cart</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+    </>
+  );
 }
