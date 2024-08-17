49988f7e668c77fa26eaf1d246fda29a4baf1665
Sharath <sharath.esr@gmail.com>
2024-08-17 19:18:18 +0530

    feat: add local JSON with product data

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import Quest from './images/quest.jpg';
+import Headphones from './images/headphones.jpg';
+import Glasses from './images/glasses.jpg';
+import Quest1 from './images/quest1.jpg';
+import Quest2 from './images/quest2.jpg';
+import CarryCase from './images/carryCase.jpg';
+import HeadStrap from './images/headStrap.jpg';
+import RingCovers from './images/ringCovers.jpg'
+import LightBlocker from './images/lightBlocker.jpg';
+import MetaQuestGiftCards from './images/metaQuestGiftCards.jpg';
+import QuestWithControllers from './images/questWithControllers.jpg';
+import LinkCable from './images/linkCable.jpg';
+import MetaQuestAdapter from './images/metaQuestAdapter.jpg';
+import QuestRightController from './images/questRightController.jpg';
+import QuestLeftController from './images/questLeftController.jpg';
+
+
+import RaybanRound from './images/raybanRound.jpg';
+import RaybanMeteor from './images/raybanMeteor.jpg';
+import RaybanWayfarer from './images/raybanWayfarer.jpg';
+import LogitechHeadphones from './images/logitechHeadphones.jpg';
+import MetaQuestProEarphones from './images/metaQuestProEarphones.jpg';
+import SoundcoreEarbuds from './images/soundcoreEarbuds.jpg';
+import Banner from './images/banner.jpg';
+
+
+export {Quest, Headphones,Glasses,Quest1,Quest2,LinkCable,QuestLeftController,QuestRightController,MetaQuestAdapter,QuestWithControllers,CarryCase,HeadStrap,MetaQuestGiftCards,RingCovers,LightBlocker,RaybanRound, RaybanMeteor,RaybanWayfarer,LogitechHeadphones,MetaQuestProEarphones,SoundcoreEarbuds,Banner};
---
+++ src/db/products.js
@@ -0,0 +1,182 @@
+
+import { v4 as uuid } from "uuid";
+import {Quest1,Quest2,CarryCase,HeadStrap,LinkCable,MetaQuestAdapter,QuestWithControllers,RingCovers,LightBlocker ,MetaQuestGiftCards,RaybanRound, RaybanMeteor,RaybanWayfarer,LogitechHeadphones,MetaQuestProEarphones,SoundcoreEarbuds,QuestLeftController,QuestRightController} from '../assets'
+
+/**
+ * Product Database can be added here.
+ * You can add products of your wish with different attributes
+ * */
+
+
+export const products = [{
+  _id: uuid(),
+  title: "Quest1",
+  originalPrice: "5000",
+  sellingPrice: "3500",
+  image: Quest1,
+  categoryName: "virtual-reality",
+  rating:"3"
+},
+{
+  _id: uuid(),
+  title: "Quest2",
+  originalPrice: "10000",
+  sellingPrice: "8500",
+  image: Quest2,
+  categoryName: "virtual-reality",
+  rating:"3.5"
+},
+
+  {
+    _id: uuid(),
+    title: "Meta Quest Pro Earphones",
+    originalPrice: "2500",
+    sellingPrice: "1000",
+    image: MetaQuestProEarphones,
+    categoryName: "soundzy",
+    rating:"4"
+  },
+  {
+    _id: uuid(),
+    title: "Logitech G PRO Gaming Headset",
+    originalPrice: "4000",
+    sellingPrice: "3000",
+    image: LogitechHeadphones,
+    categoryName: "soundzy",
+    rating:"4"
+  },
+  {
+    _id: uuid(),
+    title: "Link Cable",
+    originalPrice: "4000",
+    sellingPrice: "3000",
+    image: LinkCable,
+    categoryName: "soundzy",
+    rating:"2"
+  },
+  {
+    _id: uuid(),
+    title: "Meta Quest Adapter  ",
+    originalPrice: "3000",
+    sellingPrice: "2500",
+    image: MetaQuestAdapter  ,
+    categoryName: "virtual-reality",
+    rating:"4"
+  },
+  {
+    _id: uuid(),
+    title: "Quest With Controllers",
+    originalPrice: "10000",
+    sellingPrice: "8500",
+    image: QuestWithControllers,
+    categoryName: "virtual-reality",
+    rating:"4.5"
+  },
+  
+  {
+    _id: uuid(),
+    title: "CarryCase - for Quest",
+    originalPrice: "10000",
+    sellingPrice: "8500",
+    image: CarryCase,
+    categoryName: "virtual-reality",
+    rating:"1.5"
+  },
+  {
+    _id: uuid(),
+    title: "Quest Pro Left Controller",
+    originalPrice: "10000",
+    sellingPrice: "8500",
+    image: QuestLeftController,
+    categoryName: "virtual-reality",
+    rating: "3.5"
+  },
+  
+  
+  {
+    _id: uuid(),
+    title: "HeadStrap",
+    originalPrice: "6000",
+    sellingPrice: "4500",
+    image: HeadStrap,
+    categoryName: "virtual-reality",
+    rating: "3"
+  },
+  
+  {
+    _id: uuid(),
+    title: "Ring Covers",
+    originalPrice: "5000",
+    sellingPrice: "4500",
+    image: RingCovers,
+    categoryName: "virtual-reality",
+    rating:"4"
+  },
+  
+  {
+    _id: uuid(),
+    title: "Light Blocker ",
+    originalPrice: "3000",
+    sellingPrice: "2500",
+    image: LightBlocker ,
+    categoryName: "virtual-reality",
+    rating:"4.5"
+  },
+  
+  {
+    _id: uuid(),
+    title: "Meta Quest GiftCards ",
+    originalPrice: "3000",
+    sellingPrice: "2500",
+    image: MetaQuestGiftCards ,
+    categoryName: "virtual-reality",
+    rating: "2"
+  },
+    {
+      _id: uuid(),
+      title: "Ray-Ban Round",
+      originalPrice: "4000",
+      sellingPrice: "2500",
+      image: RaybanRound,
+      categoryName: "smart-glasses",
+      rating: "4"
+    },
+    {
+      _id: uuid(),
+      title: "Ray-Ban Meteor",
+      originalPrice: "8000",
+      sellingPrice: "6500",
+      image: RaybanMeteor,
+      categoryName: "smart-glasses",
+      rating: "3"
+    },
+
+    {
+      _id: uuid(),
+      title: "Ray-Ban Wayfarer",
+      originalPrice: "8000",
+      sellingPrice: "6500",
+      image: RaybanWayfarer,
+      categoryName: "smart-glasses",
+      rating: "3.5"
+    },
+    
+    {
+      _id: uuid(),
+      title: "Soundcore VR Earbuds",
+      originalPrice: "2500",
+      sellingPrice: "1000",
+      image: SoundcoreEarbuds,
+      categoryName: "soundzy",
+      rating: "3.5"
+    },
+    {
+      _id: uuid(),
+      title: "Quest Pro Right Controller",
+      originalPrice: "10000",
+      sellingPrice: "8500",
+      image: QuestRightController,
+      categoryName: "virtual-reality",
+      rating: "3.5"
+    }
+];
